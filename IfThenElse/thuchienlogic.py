efb06ea7ae5b7bcbf7464c468090182e85f059d7
unknown <thangvv@Ultimate-PC.ETEC.VN>
2015-09-29 13:09:10 +0700

    Add thuchienlogic.py

---
+++ IfThenElse/thuchienlogic.py
@@ -0,0 +1,16 @@
+__author__ = 'thangvv'
+
+
+score = 76
+
+if score >= 90:
+    print('grade is A')
+elif score >= 80:
+    print('grade is B')
+elif score >= 70:
+    print('grade is C')
+elif score >= 60:
+    print('grade is D')
+else:
+    print ('grade is F')
+
